97383a169b014bc24e93e83b5627f90f75e09e1f
devendiran_kumar_ <devendiran.kumar@siemens.com>
2019-12-03 16:06:31 +0530

    Create auto-updating app

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, ipcMain } = require('electron')
+const { autoUpdater } = require('electron-updater');
 const path = require('path')
 
 // Keep a global reference of the window object, if you don't, the window will
@@ -34,7 +35,19 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+
+app.on('ready', () => {
+  createWindow();
+  autoUpdater.checkForUpdatesAndNotify();
+});
+
+autoUpdater.on('update-available', () => {
+  mainWindow.webContents.send('update_available');
+});
+autoUpdater.on('update-downloaded', () => {
+  mainWindow.webContents.send('update_downloaded');
+});
+
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -51,3 +64,12 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+
+ipcMain.on('app_version', (event) => {
+  event.sender.send('app_version', { version: app.getVersion() });
+});
+
+ipcMain.on('restart_app', () => {
+  autoUpdater.quitAndInstall();
+});
